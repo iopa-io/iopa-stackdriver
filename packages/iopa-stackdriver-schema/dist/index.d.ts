70df16b03a7c6af2fbfc34d569127e009c3d646f
Guy Barnard <guy@domabo.com>
2019-11-23 10:58:13 -0600

    Remove dist from vc

--- packages/iopa-stackdriver-schema/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './generated-logging';
-export { ProjectsApi as ErrorReportingApi, ReportedErrorEvent, ReportErrorEventResponse } from './generated-error-reporting';
-export { TokenService } from './auth/token-service';
